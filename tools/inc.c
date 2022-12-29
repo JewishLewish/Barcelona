27acdac25657d6a2cf4f11611b53e9dd63a60014
JewishLewish <lanbrokhman@gmail.com>
2022-12-28 17:27:07 -0800

    Using C code to accelerate Inc.

---
+++ tools/inc.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int Incr(char a[10])
+{
+  int x = atoi(a);
+  return ++x;
+}
--- tools/strtoint.nim
+++ tools/strtoint.nim
@@ -1,4 +1,12 @@
 from strutils import parseInt
 
+{.compile: "inc.c".}
+proc Incr(a: cstring): int {.importc, varargs}
+
+
 proc PI*(n: string): int =
     return parseInt(n) 
+
+proc Cinc*(n: string): int =
+    var x: int = Incr(n)
+    return x
