e3e03af2706c99a35d559baa3720e0da4266a383
JewishLewish <lanbrokhman@gmail.com>
2022-12-28 21:15:23 -0800

    Adjustments to Benchmark.

--- modules/bm.nim
+++ modules/bm.nim
@@ -2,7 +2,7 @@ from std/times import cpuTime
 import asyncdispatch
 
 var time: float = 0
-var track = 0
+var track:int8 = 0
 proc benchmark*() {.async.}= 
     if track == 0:
         time = cpuTime()
